f9d5bae1cf969d37c537d98b723f1a7cf222e9d2
andy <ztao8607@gmail.com>
2019-05-21 09:49:09 +0800

    List Nodes

--- doctor/main.go
+++ doctor/main.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+
+	"github.com/andy-zhangtao/doctor/doctor/cli"
+	"github.com/andy-zhangtao/doctor/doctor/server"
+)
+
 /**
 doctor 是堡垒机的核心组件
 
@@ -11,5 +19,17 @@ doctor会尝试将用户的登录请求转发到远程节点。
 **/
 
 func main() {
+	isServer := flag.Bool("server", false, "Run as daemon mode")
+	isCli := flag.Bool("client", false, "Run as client mode")
 
+	flag.Parse()
+
+	if *isServer {
+		fmt.Println("===Server Mode===")
+		server.StartWeb()
+	}
+
+	if *isCli {
+		cli.Command()
+	}
 }
--- model/node.go
+++
@@ -1,9 +0,0 @@
-package model
-
-// RemoteNode 远程节点
-type RemoteNode struct {
-	IP       string
-	User     string
-	Password string
-	Key      string
-}
